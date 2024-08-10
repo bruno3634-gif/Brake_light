de0557de0a8bcac1f2a66293123ee113584dd1d2
bruno vicente <bruno.marvicente@gmail.com>
2024-08-10 11:56:55 +0100

    OTA added

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Adafruit_NeoPixel.h>
 #include <ESP32_CAN.h>
-#include <BluetoothSerial.h>
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WebServer.h>
+#include <ESP2SOTA.h>
 
 
-
+const char* ssid = "ASSI_LART_T24";
+const char* password = "T24E_aut";
+WebServer server(80);
 
 #define PIN 16           // Pino controlo LEDS
 #define NUM_LEDS 15     // Número de LEDS
@@ -16,7 +21,7 @@ int ADC_PIN = 12;
 QueueHandle_t xQueue;
 TaskHandle_t LER_ADC;
 
-BluetoothSerial bl;
+
 hw_timer_t *timer = NULL;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 void colorWipe(uint32_t color, int wait);
@@ -40,8 +45,7 @@ unsigned long send = 0;
  
 
 void setup() {
-  // put your setup code here, to run once:
-  bl.begin("Brake light");
+
   pinMode(BRAKE,INPUT);
   strip.show();
   strip.clear();
@@ -50,30 +54,38 @@ void setup() {
   Serial.begin(115200);
   timeout = millis();
 
+  /***        --- Initiate access point and configure static ip addresss as 06.14.22.24 ---         ***/
+
+  WiFi.mode(WIFI_AP);  
+  WiFi.softAP(ssid, password);
+  IPAddress IP = IPAddress (06, 14, 22, 24);
+  IPAddress NMask = IPAddress (255, 255, 255, 0);
+  IPAddress myIP = WiFi.softAPIP();
+  WiFi.softAPConfig(IP, IP, NMask);
+  ESP2SOTA.begin(&server);
+  server.begin();
+
+  server.on("/display", HTTP_GET, []() {
+    String message = "Estado da Missao: " + String(digitalRead(BRAKE));
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", message);
+  });
+
 }
 
 
 void loop() {
+  server.handleClient();
   // put your main code here, to run repeatedly:
   if(digitalRead(BRAKE))
   {
     brake = 0;
-    if(bl.available())
-    {
-      bl.println("Braking");
-    }
     colorWipe(strip.Color(255, 0, 0), 0);
 
   }
   else
   {
     colorWipe(strip.Color(0, 0, 0), 0);
-    
-  
-        if(bl.available())
-        {
-          bl.println("Not Braking");
-        }
   }
  
 
